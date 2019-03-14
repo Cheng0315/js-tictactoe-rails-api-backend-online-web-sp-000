a93eff296eeb510b9aa6aa8a5a95a264ef6ce102
Cheng Vue <csalvue@gmail.com>
2019-03-14 05:39:06 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,18 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def index
 
+  end
+
+  def created
+    @game = Game.create()
+  end
+
+  def Show
+
+  end
+
+  def update
+
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,3 @@
 class HomeController < ApplicationController
-  def index
-  end
 
-  def created
-
-  end
-
-  def Show
-
-  end
-
-  def update
-
-  end
 end
